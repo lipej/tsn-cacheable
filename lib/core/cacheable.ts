f794143d8c1c1153be349bd43c4cf555dbaf9140
Felipe José <lipej@pm.me>
2023-03-03 11:35:26 -0300

    fix: typo

--- lib/core/cacheable.ts
+++ lib/core/cacheable.ts
@@ -1,15 +1,15 @@
 import { KEY_SUFFIX } from "../application/config/guards";
 import { RepositoryInstance } from "../application/singletons/repository-instance";
 
-export function Cacheable(keyPreffix: string, index = 0) {
+export function Cacheable(keyPrefix: string, index = 0) {
     const repository = RepositoryInstance.get()
     return (_target: unknown, _key: string, propDesc: PropertyDescriptor): any => {
         const originalFunction = propDesc.value as () => void
         propDesc.value = async function () {
-            const keySuffix = arguments[index]
-            if (!KEY_SUFFIX.allowed.includes(typeof keySuffix)) throw new Error(KEY_SUFFIX.error)
+            const keySufix = arguments[index]
+            if (!KEY_SUFFIX.allowed.includes(typeof keySufix)) throw new Error(KEY_SUFFIX.error)
 
-            const cacheKey = keyPreffix.concat('-', keySuffix)
+            const cacheKey = keyPrefix.concat('-', keySufix)
             const value = await repository.get(cacheKey)
 
             return value ? value : originalFunction.apply(this, arguments).then(async (result: any) => {
